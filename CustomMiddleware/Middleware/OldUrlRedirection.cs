20d00ebd31a54678a0aa41dc16a71e591de2fe29
SHASHIKANT-SHAH <shashikantshah159@gmail.com>
2023-01-29 00:16:03 +0530

    Custom Middleware

---
+++ CustomMiddleware/Middleware/OldUrlRedirection.cs
@@ -0,0 +1,57 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+using System.Threading.Tasks;
+
+namespace CustomMiddleware.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class OldUrlRedirection
+    {
+        private readonly RequestDelegate _next;
+
+        public OldUrlRedirection(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+
+            var url = httpContext.Request.Path.Value;
+            string redirectUrl = "";
+            if (!string.IsNullOrEmpty(url))
+            {
+                url = url.ToLowerInvariant();
+            }
+            switch (url)
+            {
+                case "/home/privacy":
+                    redirectUrl = "/privacy-policy";
+                    break;
+                case "/account/login":
+                    redirectUrl = "/login";
+                    break;
+            }
+
+           if (!string.IsNullOrEmpty(redirectUrl))
+            {
+                httpContext.Response.Redirect(redirectUrl);
+                return;
+            }
+            // Call the next middleware in the pipeline
+            await _next(httpContext);
+
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class OldUrlRedirectionExtensions
+    {
+        public static IApplicationBuilder UseOldUrlRedirection(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<OldUrlRedirection>();
+        }
+    }
+}
